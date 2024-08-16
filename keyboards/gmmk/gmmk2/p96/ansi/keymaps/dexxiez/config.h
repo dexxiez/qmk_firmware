5f761d81de81248dfa0e57d73e878c7c23d5f656
Toby Boulton <toby@cosmictiger.com.au>
2024-08-16 22:56:46 +1000

    home keymap caps rgb

---
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/dexxiez/config.h
@@ -0,0 +1,12 @@
+
+#define RGB_BG 227, 28, 121
+#define RGB_INSERT 0, 255, 0
+#define RGB_DELETE 255, 140, 0
+
+#define ESC_KEY 0
+#define INSERT_KEY 13
+#define DELETE_KEY 14
+#define NUMLOCK_KEY 32
+#define CAPSLOCK_KEY 54
+
+#define MEDIA_KEYS_RANGE 5, 6, 7, 8
--- keyboards/gmmk/gmmk2/p96/ansi/keymaps/dexxiez/keymap.c
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/dexxiez/keymap.c
@@ -20,6 +20,7 @@
 //////////////////// :noa w ////////////////////
 
 #include QMK_KEYBOARD_H
+#include "config.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -33,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 [_BL] = LAYOUT(
   QK_GESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_DEL,   KC_PSCR,  KC_PGDN,  KC_PGUP,
-  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_PEQL,  KC_PSLS,  KC_PAST,  KC_PMNS,
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
   QK_GESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   KC_P4,    KC_P5,    KC_P6,
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
@@ -43,18 +44,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_FL] = LAYOUT(
   QK_BOOT,  EE_CLR,  KC_WHOM,  KC_CALC,  KC_MSEL,  KC_MPRV,  KC_MRWD,  KC_MPLY,  KC_MSTP,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,   _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  KC_NUM,  _______,  _______,  _______,
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,
   KC_CAPS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,             _______,  _______,  _______,
   _______,  RGB_HUI,  RGB_HUD,  RGB_SPD,  RGB_SPI,  QK_BOOT,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_VAI,   _______,  _______,  _______,  _______,
   _______,  UC_WIN,   _______,                      _______,                                _______,  _______,  _______,  RGB_RMOD,  RGB_VAD,  RGB_MOD,  _______,  _______)
 };
 
-#define RGB_BG 227, 28, 121
-
-static int MEDIA_KEYS[]      = {5, 6, 7, 8};
-static int CAPS_LOCK_KEY     = 54;
+static int MEDIA_KEYS[]      = {MEDIA_KEYS_RANGE};
 static int MEDIA_KEYS_LENGTH = sizeof(MEDIA_KEYS) / sizeof(MEDIA_KEYS[0]);
+
+bool numlock_active = false;
+bool capslock_active = false;
+
+void handle_keystates(void){
+    if (!numlock_active){
+        rgb_matrix_set_color(NUMLOCK_KEY, RGB_RED);
+    }
+
+    if (capslock_active){
+        rgb_matrix_set_color(CAPSLOCK_KEY, RGB_GREEN);
+    }
+}
 
 bool rgb_matrix_indicators_kb(void) {
     // If layer _FL is active
@@ -66,24 +77,43 @@ bool rgb_matrix_indicators_kb(void) {
             rgb_matrix_set_color(MEDIA_KEYS[i], RGB_BG);
         }
 
-        rgb_matrix_set_color(CAPS_LOCK_KEY, RGB_BG);
+        rgb_matrix_set_color(CAPSLOCK_KEY, RGB_BG);
 
         return true;
     }
 
     // Background color
     rgb_matrix_set_color_all(RGB_BG);
-    rgb_matrix_set_color(0, RGB_BG); // Escape key because it's not covered by all for some reason
+    rgb_matrix_set_color(ESC_KEY, RGB_BG); // Escape key because it's not covered by all for some reason
 
     // Insert key
-    rgb_matrix_set_color(13, 0, 255, 0);
+    rgb_matrix_set_color(INSERT_KEY, RGB_INSERT);
 
     // Delete key
-    rgb_matrix_set_color(14, 255, 140, 0);
+    rgb_matrix_set_color(DELETE_KEY, RGB_DELETE);
 
+    return true;
+}
+
+bool led_update_kb(led_t led_state){
+    bool res = led_update_user(led_state);
+    if(res){
+        numlock_active = led_state.num_lock;
+        capslock_active = led_state.caps_lock;
+        handle_keystates();
+    }
+    return res;
+}
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    handle_keystates();
     return true;
 }
 
 void keyboard_post_init_user(void) {
     rgb_matrix_set_color_all(60, 0, 127);
+
+    numlock_active = host_keyboard_led_state().num_lock;
+    capslock_active = host_keyboard_led_state().caps_lock;
+    handle_keystates();
 }
