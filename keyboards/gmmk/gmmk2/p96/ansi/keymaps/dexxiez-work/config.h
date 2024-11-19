b65d83b90eb94c44f48b75665d6b3c51e7d6198b
Toby Boulton <toby@cosmictiger.com.au>
2024-11-20 07:08:34 +1100

    feat(keyboard): add RGB background color cycling with EEPROM persistence
    - Add multiple predefined RGB background colors (blue, peach, pink, purple)
    - Implement background color cycling via custom keycode on function layer
    - Add EEPROM support to persist selected background color
    - Create RGB color structure definition
    - Add header guards to config files

--- keyboards/gmmk/gmmk2/p96/ansi/keymaps/dexxiez-work/config.h
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/dexxiez-work/config.h
@@ -1,5 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
-#define RGB_BG 204, 86, 3
+#define RGB_BG_BLUE 7, 181, 181
+#define RGB_BG_PEACH 204, 86, 3
+#define RGB_BG_PINK_2 189, 4, 142
+#define RGB_BG_PURPLE 83, 7, 181
+
 #define RGB_INSERT 0, 255, 0
 #define RGB_DELETE 255, 140, 0
 
@@ -10,3 +16,7 @@
 #define CAPSLOCK_KEY 54
 
 #define MEDIA_KEYS_RANGE 5, 6, 7, 8
+
+#define EEPROM_BG_COLOR_INDEX 32
+
+#endif
---
+++ keyboards/gmmk/gmmk2/p96/ansi/structs.h
@@ -0,0 +1,10 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} rgb_color;
+
+#endif
